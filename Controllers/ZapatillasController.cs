a8c20a7831ab644405d3b4b11becea0c764e16f3
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-15 11:55:13 +0100

    11:55

--- Controllers/ZapatillasController.cs
+++ Controllers/ZapatillasController.cs
@@ -21,5 +21,68 @@ namespace PracticaZapatillas.Controllers
             Zapatilla zapatilla = await this.repo.FindZapatillaAsync(idZapatilla);
             return View(zapatilla);
         }
+        public async Task<IActionResult> ImagenZapatilla
+            (int? posicion, int idZapatilla)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            ImagenPaginacion model = await this.repo.
+                GetZapatillaImagenAsync(posicion.Value, idZapatilla);
+            Zapatilla zapatilla = await this.repo.FindZapatillaAsync(idZapatilla);
+            ViewData["ZAPATILLASELECCIONADA"] = zapatilla;
+            ViewData["REGISTROS"] = model.Registros;
+            ViewData["ZAPATILLA"] = idZapatilla;
+
+            int siguiente = posicion.Value + 1;
+            if(siguiente > model.Registros)
+            {
+                siguiente = model.Registros;
+            }
+
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = model.Registros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            ViewData["POSICION"] = posicion;
+            return View(model.Imagen);
+        }        
+        
+        public async Task<IActionResult> _ZapatillaPartial
+            (int? posicion, int idZapatilla)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            ImagenPaginacion model = await this.repo.
+                GetZapatillaImagenAsync(posicion.Value, idZapatilla);
+            Zapatilla zapatilla = await this.repo.FindZapatillaAsync(idZapatilla);
+            ViewData["ZAPATILLASELECCIONADA"] = zapatilla;
+            ViewData["REGISTROS"] = model.Registros;
+            ViewData["ZAPATILLA"] = idZapatilla;
+
+            int siguiente = posicion.Value + 1;
+            if(siguiente > model.Registros)
+            {
+                siguiente = model.Registros;
+            }
+
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = model.Registros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            ViewData["POSICION"] = posicion;
+            return PartialView("_ZapatillaPartial", model.Imagen);
+        }
     }
 }
